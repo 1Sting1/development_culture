449037cbdcb5c1564be81fcc1575ffb55f1cc512
sting <maks.kotov38@gmail.com>
2025-03-13 22:44:40 +0800

    add_new

---
+++ docs/conf.py
@@ -0,0 +1,23 @@
+# Configuration file for the Sphinx documentation builder.
+
+# -- Project information -----------------------------------------------------
+project = 'Development Culture'
+copyright = 'Maksim Kotelnikov'
+author = 'Maksim Kotelnikov'
+
+# -- General configuration ---------------------------------------------------
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.viewcode',
+    'sphinx.ext.napoleon',
+]
+
+templates_path = ['_templates']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+# -- Options for HTML output -------------------------------------------------
+html_theme = 'sphinx_rtd_theme'
+html_static_path = ['_static']
+
+# -- Language settings -------------------------------------------------------
+language = 'ru'
